63559e5e167526283009518aec47535aac035cbc
Patrick Snape <patricksnape@gmail.com>
2015-02-02 12:37:39 +0000

    Fix viewing for the new viewing API
    This doesn't include any nice parameter API as has been added for
    the 2D viewers. This just means that viewing is actually possible
    for 3D.

--- menpo3d/visualize/base.py
+++ menpo3d/visualize/base.py
@@ -1,9 +1,10 @@
 from .viewmayavi import (
-    MayaviPointCloudViewer3d, MayaviTriMeshViewer3d,
+    MayaviPointCloudViewer3d, MayaviTriMeshViewer3d, MayaviPointGraphViewer3d,
     MayaviTexturedTriMeshViewer3d, MayaviLandmarkViewer3d,
     MayaviVectorViewer3d, MayaviColouredTriMeshViewer3d)
 
 PointCloudViewer3d = MayaviPointCloudViewer3d
+PointGraphViewer3d = MayaviPointGraphViewer3d
 TriMeshViewer3d = MayaviTriMeshViewer3d
 TexturedTriMeshViewer3d = MayaviTexturedTriMeshViewer3d
 ColouredTriMeshViewer3d = MayaviColouredTriMeshViewer3d
--- menpo3d/visualize/viewmayavi.py
+++ menpo3d/visualize/viewmayavi.py
@@ -1,4 +1,3 @@
-import abc
 import numpy as np
 from menpo.visualize.base import Renderer
 
@@ -16,8 +15,6 @@ class MayaviViewer(Renderer):
         If `True`, creates a new figure to render on.
     """
 
-    __metaclass__ = abc.ABCMeta
-
     def __init__(self, figure_id, new_figure):
         try:
             import mayavi
@@ -25,6 +22,14 @@ class MayaviViewer(Renderer):
             raise ImportError("mayavi is required for viewing 3D objects "
                               "(consider 'conda/pip install mayavi')")
         super(MayaviViewer, self).__init__(figure_id, new_figure)
+
+        self._supported_ext = ['.png', '.jpg', '.bmp', '.tiff',  # 2D
+                               '.ps', '.eps', '.pdf',  # 2D
+                               '.rib', '.oogl', 'iv', '.vrml', '.obj']  # 3D
+        n_ext = len(self._supported_ext)
+        func_list = [lambda obj, fp: mayavi.mlab.savefig(fp, **obj)] * n_ext
+        self._extensions_map = dict(zip(['.' + s for s in self._supported_ext],
+                                    func_list))
 
     def get_figure(self):
         r"""
@@ -38,7 +43,7 @@ class MayaviViewer(Renderer):
         figure : Mayavi figure object
             The figure we will be rendering on.
         """
-        from mayavi import mlab
+        import mayavi.mlab as mlab
         if self.new_figure or self.figure_id is not None:
             self.figure = mlab.figure(self.figure_id)
         else:
@@ -47,6 +52,37 @@ class MayaviViewer(Renderer):
         self.figure_id = self.figure.name
 
         return self.figure
+
+    def save_figure(self, filename, format='png', size=None,
+                    magnification='auto', overwrite=False):
+        r"""
+        Method for saving the figure of the current `figure_id` to file.
+
+        Parameters
+        ----------
+        filename : `str` or `file`-like object
+            The string path or file-like object to save the figure at/into.
+        format : `str`
+            The format to use. This must match the file path if the file path is
+            a `str`.
+        size : `tuple` of `int`, optional
+            The size of the image created (unless magnification is set,
+            in which case it is the size of the window used for rendering).
+        magnification :	`double`, optional
+            The magnification is the scaling between the pixels on the screen,
+            and the pixels in the file saved. If you do not specify it, it will
+            be calculated so that the file is saved with the specified size.
+            If you specify a magnification, Mayavi will use the given size as a
+            screen size, and the file size will be ``magnification * size``.
+        overwrite : `bool`, optional
+            If ``True``, the file will be overwritten if it already exists.
+        """
+        from menpo.io.output.base import _export
+        savefig_args = {'size': size, 'figure': self.figure,
+                        'magnification': magnification}
+        # Use the export code so that we have a consistent interface
+        _export(savefig_args, filename, self._extensions_map, format,
+                overwrite=overwrite)
 
     @property
     def width(self):
@@ -108,11 +144,39 @@ class MayaviPointCloudViewer3d(MayaviViewer):
         super(MayaviPointCloudViewer3d, self).__init__(figure_id, new_figure)
         self.points = points
 
-    def _render(self, **kwargs):
+    def render(self, marker_size=1, marker_face_colour=(1, 1, 1), **kwargs):
         from mayavi import mlab
         mlab.points3d(
             self.points[:, 0], self.points[:, 1], self.points[:, 2],
-            figure=self.figure, scale_factor=1)
+            figure=self.figure, scale_factor=marker_size,
+            color=marker_face_colour)
+        return self
+
+
+class MayaviPointGraphViewer3d(MayaviViewer):
+
+    def __init__(self, figure_id, new_figure, points, adjacency_array):
+        super(MayaviPointGraphViewer3d, self).__init__(figure_id, new_figure)
+        self.points = points
+        self.adjacency_array = adjacency_array
+
+    def render(self, line_width=4, render_points=True, points_color=(0, 0, 1),
+               points_scale=2, line_color=(0, 0, 1), **kwargs):
+        from mayavi import mlab
+        # Create the points
+        src = mlab.pipeline.scalar_scatter(self.points[:, 0], self.points[:, 1],
+                                           self.points[:, 2])
+        # Connect them
+        src.mlab_source.dataset.lines = self.adjacency_array
+        # The stripper filter cleans up connected lines
+        lines = mlab.pipeline.stripper(src)
+
+        # Finally, display the set of lines
+        mlab.pipeline.surface(lines, line_width=line_width, color=line_color)
+        if render_points:
+            mlab.points3d(self.points[:, 0], self.points[:, 1],
+                          self.points[:, 2], scale_factor=points_scale,
+                          color=points_color)
         return self
 
 
@@ -124,7 +188,7 @@ class MayaviSurfaceViewer3d(MayaviViewer):
             values[~mask] = np.nan
         self.values = values
 
-    def _render(self, **kwargs):
+    def render(self, **kwargs):
         from mayavi import mlab
         warp_scale = kwargs.get('warp_scale', 'auto')
         mlab.surf(self.values, warp_scale=warp_scale)
@@ -133,27 +197,26 @@ class MayaviSurfaceViewer3d(MayaviViewer):
 
 class MayaviLandmarkViewer3d(MayaviViewer):
 
-    def __init__(self, figure_id, new_figure, group_label, pointcloud,
-                 labels_to_masks):
+    def __init__(self, figure_id, new_figure, pointcloud, lmark_group):
         super(MayaviLandmarkViewer3d, self).__init__(figure_id, new_figure)
-        self.label = group_label
         self.pointcloud = pointcloud
-        self.labels_to_masks = labels_to_masks
+        self.lmark_group = lmark_group
 
-    def _render(self, **kwargs):
-        from mayavi import mlab
+    def render(self, scale_factor=1.0, text_scale=1.0, **kwargs):
+        import mayavi.mlab as mlab
         # disabling the rendering greatly speeds up this for loop
         self.figure.scene.disable_render = True
         positions = []
-        for label, mask in self.labels_to_masks.iteritems():
-            p = self.pointcloud.from_mask(mask)
+        for label in self.lmark_group:
+            p = self.lmark_group[label]
             for i, p in enumerate(p.points):
                 positions.append(p)
                 l = '%s_%d' % (label, i)
                 # TODO: This is due to a bug in mayavi that won't allow
                 # rendering text to an empty figure
-                mlab.points3d(p[0], p[1], p[2])
-                mlab.text3d(p[0], p[1], p[2], l, figure=self.figure)
+                mlab.points3d(p[0], p[1], p[2], scale_factor=scale_factor)
+                mlab.text3d(p[0], p[1], p[2], l, figure=self.figure,
+                            scale=text_scale)
         positions = np.array(positions)
         os = np.zeros_like(positions)
         os[:, 2] = 1
@@ -172,7 +235,7 @@ class MayaviTriMeshViewer3d(MayaviViewer):
         self.trilist = trilist
 
     def _render_mesh(self):
-        from mayavi import mlab
+        import mayavi.mlab as mlab
         mlab.triangular_mesh(self.points[:, 0],
                              self.points[:, 1],
                              self.points[:, 2],
@@ -180,7 +243,7 @@ class MayaviTriMeshViewer3d(MayaviViewer):
                              color=(0.5, 0.5, 0.5),
                              figure=self.figure)
 
-    def _render(self, normals=None, **kwargs):
+    def render(self, normals=None, **kwargs):
         if normals is not None:
             MayaviVectorViewer3d(self.figure_id, False,
                                  self.points, normals)._render(**kwargs)
@@ -248,7 +311,7 @@ class MayaviVectorViewer3d(MayaviViewer):
         self.points = points
         self.vectors = vectors
 
-    def _render(self, **kwargs):
+    def render(self, **kwargs):
         from mayavi import mlab
         # Only get every nth vector. 1 means get every vector.
         mask_points = kwargs.get('mask_points', 1)
