b214423f6888535a79377463479232618356a171
James Booth <jabooth@gmail.com>
2015-06-25 16:25:05 +0100

    clean up setup.py for new versioneer

--- setup.py
+++ setup.py
@@ -2,16 +2,6 @@ from setuptools import setup, find_packages
 import sys
 import versioneer
 
-project_name = 'menpo3d'
-
-# Versioneer allows us to automatically generate versioning from
-# our git tagging system which makes releases simpler.
-versioneer.VCS = 'git'
-versioneer.versionfile_source = '{}/_version.py'.format(project_name)
-versioneer.versionfile_build = '{}/_version.py'.format(project_name)
-versioneer.tag_prefix = 'v'  # tags are like v1.2.0
-versioneer.parentdir_prefix = project_name + '-'  # dirname like 'menpo-v1.2.0'
-
 install_requires = ['menpo>=0.4.0,<0.5',
                     'cyassimp>=0.2,<0.3',
                     'cyrasterize>=0.2,<0.3']
@@ -21,7 +11,7 @@ if sys.version_info.major == 2:
     install_requires.append('mayavi>=4.4,<4.5')
     install_requires.append('menpo-pyvrml97==2.3.0a4')
 
-setup(name=project_name,
+setup(name='menpo3d',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       description='MenpoKit providing tools for 3D Computer Vision research',
