a26f3ec1e0c196ad57508f3734f53e526f5a1c5d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-09-21 11:49:26 -0400

    Support for PLY format (#31)
    * Initial import working for nontextured meshes
    
    * Export working
    
    * Clean up debug messages
    
    * Allow path only exporters
    
    This requires https://github.com/menpo/menpo/pull/737
    to be merged.
    
    * Fix export tests
    
    Mild changes to ply exporter

--- menpo3d/io/test/io_export_test.py
+++ menpo3d/io/test/io_export_test.py
@@ -1,9 +1,24 @@
+import contextlib
+import os
+import tempfile
 from mock import patch, PropertyMock, MagicMock
 import menpo3d.io as mio
 
 
 test_obj = mio.import_builtin_asset('james.obj')
 test_lg = mio.import_landmark_file(mio.data_path_to('bunny.ljson'))
+
+
+@contextlib.contextmanager
+def _temporary_path(extension):
+    # Create a temporary file and remove it
+    fake_path = tempfile.NamedTemporaryFile(delete=False, suffix=extension)
+    fake_path.close()
+    fake_path = fake_path.name
+    os.unlink(fake_path)
+    yield fake_path
+    if os.path.exists(fake_path):
+        os.unlink(fake_path)
 
 
 @patch('menpo3d.io.output.base.Path.exists')
@@ -14,6 +29,18 @@ def test_export_mesh_obj(mock_open, exists):
     with open(fake_path) as f:
         type(f).name = PropertyMock(return_value=fake_path)
         mio.export_mesh(test_obj, f, extension='obj')
+
+
+def test_export_mesh_ply_ascii():
+    with _temporary_path('.ply') as f:
+        mio.export_mesh(test_obj, f)
+        assert os.path.exists(f)
+
+
+def test_export_mesh_ply_binary():
+    with _temporary_path('.ply') as f:
+        mio.export_mesh(test_obj, f, binary=True)
+        assert os.path.exists(f)
 
 
 @patch('PIL.Image.EXTENSION')
@@ -27,6 +54,19 @@ def test_export_mesh_obj_textured(mock_open, exists, PILImage, PIL):
     fake_path = '/fake/fake.obj'
     mio.export_textured_mesh(test_obj, fake_path, extension='obj')
     assert PILImage.fromarray.called
+
+
+@patch('PIL.Image.EXTENSION')
+@patch('menpo.image.base.PILImage')
+@patch('menpo3d.io.output.base.Path.exists')
+@patch('menpo.io.output.base.Path.open')
+def test_export_mesh_ply_textured(mock_open, exists, PILImage, PIL):
+    PIL.return_value.Image.EXTENSION = {'.jpg': None}
+    exists.return_value = False
+    mock_open.return_value = MagicMock()
+    with _temporary_path('.ply') as f:
+        mio.export_textured_mesh(test_obj, f)
+        assert PILImage.fromarray.called
 
 
 @patch('menpo.io.output.landmark.json.dump')
