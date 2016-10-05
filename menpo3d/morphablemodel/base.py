e1f01180dfc269d681d1ab735d2f672cc397a319
James Booth <jabooth@gmail.com>
2016-10-05 13:35:39 +0100

    rename morphable model class, move to base module

--- menpo3d/morphablemodel/base.py
+++ menpo3d/morphablemodel/base.py
@@ -1,33 +1,20 @@
 import numpy as np
-import menpo.io as mio
-import menpo3d.io as m3dio
 from menpo.shape import ColouredTriMesh
 from menpo.transform import UniformScale
 
 
-class Menpo3DMM:
+class ColouredMorphableModel(object):
     
     def __init__(self, shape_model=None, texture_model=None, landmarks=None):
         self.shape_model = shape_model
         self.texture_model = texture_model
         self.landmarks = landmarks
 
-    @classmethod
-    def init_from_file(cls, shape_path, texture_path, landmarks_path=None):
-        shape = mio.import_pickle(shape_path)
-        texture = mio.import_pickle(texture_path)
-        if landmarks_path is not None:
-            landmarks = m3dio.import_landmark_file(landmarks_path).lms
-        else:
-            landmarks = None
-        return Menpo3DMM(shape, texture, landmarks)
-    
     def __str__(self):
-        str_out = '3D MORPHABLE MODEL \n\n' + \
-                  'Shape Model \n----------- \n' + self.shape_model.__str__() + '\n' + \
-                  'Texture Model \n------------- \n' + self.texture_model.__str__() + '\n' + \
-                  'Landmarks \n--------- \n' + self.landmarks.__str__()
-        return str_out
+        return ('ColouredMorphableModel\n\n' +
+                'Shape Model \n----------- \n' + self.shape_model.__str__() + '\n' +
+                'Colour Model \n------------- \n' + self.texture_model.__str__() + '\n' +
+                'Landmarks \n--------- \n' + self.landmarks.__str__())
 
     def instance(self, model_type = 'bfm', alpha=None, beta=None, landmark_group = 'ibug68'):
         if alpha is None:
