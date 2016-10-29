145a84023ef1f489cebf185831bc394d41b75f9a
James Booth <jabooth@gmail.com>
2016-10-29 13:36:03 +0100

    CPU-based rasterization

--- setup.py
+++ setup.py
@@ -1,5 +1,65 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 import versioneer
+import os
+import pkg_resources
+import platform
+
+SYS_PLATFORM = platform.system().lower()
+IS_LINUX = 'linux' in SYS_PLATFORM
+IS_OSX = 'darwin' == SYS_PLATFORM
+IS_WIN = 'windows' == SYS_PLATFORM
+# Get Numpy include path without importing it
+NUMPY_INC_PATH = pkg_resources.resource_filename('numpy', 'core/include')
+
+
+# ---- C/C++ EXTENSIONS ---- #
+# Stolen (and modified) from the Cython documentation:
+#     http://cython.readthedocs.io/en/latest/src/reference/compilation.html
+def no_cythonize(extensions, **_ignore):
+    import os.path as op
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in ('.pyx', '.py'):
+                if extension.language == 'c++':
+                    ext = '.cpp'
+                else:
+                    ext = '.c'
+                sfile = path + ext
+                if not op.exists(sfile):
+                    raise ValueError('Cannot find pre-compiled source file '
+                                     '({}) - please install Cython'.format(sfile))
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
+
+def build_extension_from_pyx(pyx_path, extra_sources_paths=None):
+    if extra_sources_paths is None:
+        extra_sources_paths = []
+    extra_sources_paths.insert(0, pyx_path)
+    ext = Extension(name=pyx_path[:-4].replace('/', '.'),
+                    sources=extra_sources_paths,
+                    include_dirs=[NUMPY_INC_PATH],
+                    language='c++')
+    if IS_LINUX or IS_OSX:
+        ext.extra_compile_args.append('-Wno-unused-function')
+    return ext
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    import warnings
+    cythonize = no_cythonize
+    warnings.warn('Unable to import Cython - attempting to build using the '
+                  'pre-compiled C++ files.')
+
+
+cython_modules = [
+    build_extension_from_pyx('menpo3d/rasterize/tripixel.pyx'),
+]
+cython_exts = cythonize(cython_modules, quiet=True)
 
 install_requires = ['menpo>=0.7,<0.8',
                     'cyrasterize>=0.3,<0.4',
@@ -14,5 +74,6 @@ setup(name='menpo3d',
       packages=find_packages(),
       package_data={'menpo3d': ['data/*']},
       install_requires=install_requires,
-      tests_require=['nose', 'mock']
-)
+      tests_require=['nose', 'mock'],
+      ext_modules=cython_exts
+      )
