fix gradient fudge factor

menpo3d/morphablemodel/algorithm/lk.py
@@ -138,15 +138,9 @@ class LucasKanade(object):
         # Compute the gradient of the image
         grad = fast_gradient(image)
 
-        # Scale the gradient by the image resolution
-        if image.shape[1] > image.shape[0]:
-            scale = image.shape[1] / 2
-        else:
-            scale = image.shape[0] / 2
-        #scale = 1
         # Create gradient image for X and Y
-        grad_y = Image(grad.pixels[:self.n_channels] * scale)
-        grad_x = Image(grad.pixels[self.n_channels:] * scale)
+        grad_y = Image(grad.pixels[:self.n_channels])
+        grad_x = Image(grad.pixels[self.n_channels:])
 
         return grad_x, grad_y
 
@@ -242,7 +236,6 @@ class Simultaneous(LucasKanade):
         # Retrieve warp (camera) parameters from the provided view and
         # projection transforms.
         camera_parameters = camera.as_vector()
-        camera_parameters[0] = 2.0 * max(image.width, image.height) / image.width
         shape_parameters = self.model.shape_model.project(instance)
         texture_parameters = self.model.project_instance_on_texture_model(instance)
 
@@ -333,7 +326,7 @@ class Simultaneous(LucasKanade):
             # Update costs
             eps = cost_closure(sd_error_img)
             if return_costs:
-                costs.append(eps)
+                costs.append(cost_closure(img_error_uv.ravel()))
 
             # Compute increment
             delta_s = - np.linalg.solve(h, sd_error_img)

menpo3d/morphablemodel/base.py
@@ -290,7 +290,7 @@ class ColouredMorphableModel(MorphableModel):
         texture_instance = texture_instance.reshape([-1, self.n_channels])
 
         # restrict the texture to 0-1
-        texture_instance = np.clip(texture_instance, 0, 1)
+        # texture_instance = np.clip(texture_instance, 0, 1)
 
         # Create trimesh
         trimesh = ColouredTriMesh(shape_instance.points,
@@ -369,7 +369,7 @@ class TexturedMorphableModel(MorphableModel):
         # Create trimesh
 
         # restrict the texture to 0-1
-        texture_instance.pixels = np.clip(texture_instance.pixels, 0, 1)
+        # texture_instance.pixels = np.clip(texture_instance.pixels, 0, 1)
 
         trimesh = TexturedTriMesh(shape_instance.points,
                                   trilist=shape_instance.trilist,