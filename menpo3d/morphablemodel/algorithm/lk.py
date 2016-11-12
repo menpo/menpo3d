08f63a13c40268295e9cd6b8dbca4d9624fe766b
Epameinondas Antonakos <antonakosn@gmail.com>
2016-11-12 14:53:38 +0000

    first definition of Wiberg; IT DOES NOT WORK

--- menpo3d/morphablemodel/algorithm/lk.py
+++ menpo3d/morphablemodel/algorithm/lk.py
@@ -90,12 +90,12 @@ class LucasKanade(object):
 
     def compute_hessian(self, sd):
         n_params = sd.shape[1]
-        sd = np.transpose(sd,  (1, 0, 2)).reshape(n_params, -1)
+        sd = np.transpose(sd, (1, 0, 2)).reshape(n_params, -1)
         return sd.dot(sd.T)
 
     def compute_sd_error(self, sd, error_uv):
         n_params = sd.shape[1]
-        sd = np.transpose(sd,  (1, 0, 2)).reshape(n_params, -1)
+        sd = np.transpose(sd, (1, 0, 2)).reshape(n_params, -1)
         error_uv = error_uv.ravel()
         return sd.dot(error_uv)
 
@@ -440,6 +440,232 @@ class SimultaneousForwardAdditive(LucasKanade):
 
     def __str__(self):
         return "Simultaneous Forward Additive"
+
+
+class WibergForwardAdditive(LucasKanade):
+    r"""
+    Class for defining Wiberg Forward Additive Morphable Model optimization
+    algorithm.
+    """
+    def run(self, image, initial_mesh, camera, gt_mesh=None, max_iters=20,
+            camera_update=False, focal_length_update=False,
+            shape_prior_weight=1., texture_prior_weight=1., landmarks=None,
+            landmarks_prior_weight=1., return_costs=False, verbose=True):
+        # Parse landmarks prior options
+        if landmarks is None or landmarks_prior_weight is None:
+            landmarks_prior_weight = None
+            landmarks = None
+        if landmarks is not None:
+            lms_points = landmarks.points[:, [1, 0]]
+
+        # Retrieve camera parameters from the provided camera object.
+        # Project provided instance to retrieve shape and texture parameters.
+        camera_parameters = camera.as_vector()
+        shape_parameters = self.model.shape_model.project(initial_mesh)
+        texture_parameters = self.model.project_instance_on_texture_model(
+            initial_mesh)
+
+        # Reconstruct provided instance
+        instance = self.model.instance(shape_weights=shape_parameters,
+                                       texture_weights=texture_parameters)
+
+        # Compute input image gradient
+        grad_x, grad_y = self.gradient(image)
+
+        # Initialize lists
+        shape_parameters_per_iter = [shape_parameters]
+        texture_parameters_per_iter = [texture_parameters]
+        camera_per_iter = [camera]
+        instance_per_iter = [instance.with_rescaled_texture(0., 1.)]
+        costs = None
+        if return_costs:
+            costs = []
+
+        # Initialize iteration counter and epsilon
+        k = 0
+        eps = np.Inf
+
+        # Main loop
+        while k < max_iters and eps > self.eps:
+            if verbose:
+                print_dynamic("{}/{}".format(k + 1, max_iters))
+                print()
+            # Apply camera projection on current instance
+            instance_in_image = camera.apply(instance)
+
+            # Compute indices locations for sampling
+            (vertex_indices, tri_indices,
+             b_coords, yx) = self.visible_sample_points(instance_in_image,
+                                                        image.shape)
+
+            # Warp the mesh with the view matrix (rotation + translation)
+            instance_w = camera.view_transform.apply(instance.points)
+
+            # Sample all the terms from the model part at the sample locations
+            warped_uv = self.sample(instance_w, vertex_indices, b_coords)
+            m_texture_uv = self.model.instance().sample_texture_with_barycentric_coordinates(b_coords, tri_indices)
+            shape_pc_uv = self.sample(self.shape_pc, vertex_indices, b_coords)
+            # Reshape shape basis after sampling
+            shape_pc_uv = shape_pc_uv.reshape([self.n_samples, 3, -1])
+
+            # Sample all the terms from the image part at the sample locations
+            img_uv = image.sample(yx)
+            grad_x_uv = grad_x.sample(yx)
+            grad_y_uv = grad_y.sample(yx)
+
+            # Compute error
+            img_error_uv = img_uv - m_texture_uv.T
+
+            # Compute Jacobian, SD and Hessian of data term
+            sd, n_camera_parameters = self.J_data(
+                camera, warped_uv, shape_pc_uv, grad_x_uv, grad_y_uv,
+                camera_update, focal_length_update)
+            hessian = sd.T.dot(sd)
+            sd_error = sd.T.dot(img_error_uv.ravel())
+
+            # Compute Jacobian, update SD and Hessian wrt shape prior
+            if shape_prior_weight is not None:
+                sd_shape = shape_prior_weight * self.J_shape_prior
+                hessian[:self.n, :self.n] += np.diag(sd_shape)
+                sd_error[:self.n] += sd_shape * shape_parameters
+
+            # Compute Jacobian, update SD and Hessian wrt landmarks prior
+            lms_error = None
+            if landmarks_prior_weight is not None:
+                # Get projected instance on landmarks and error term
+                warped_lms = instance_in_image.points[
+                    self.model.model_landmarks_index]
+                lms_error = (warped_lms[:, [1, 0]] - lms_points).T
+
+                # Jacobian and Hessian wrt shape parameters
+                warped_view_lms = instance_w[self.model.model_landmarks_index]
+                sd_lms_shape = d_camera_d_shape_parameters(
+                    camera, warped_view_lms, self.shape_pc_lms)
+                hessian[:self.n, :self.n] += (
+                    landmarks_prior_weight * self.compute_hessian(sd_lms_shape))
+                sd_error[:self.n] += (
+                    landmarks_prior_weight * self.compute_sd_error(sd_lms_shape,
+                                                                   lms_error))
+
+                # Jacobian and Hessian wrt camera parameters
+                if camera_update:
+                    sd_lms_camera = d_camera_d_camera_parameters(
+                        camera, warped_view_lms,
+                        with_focal_length=focal_length_update)
+                    hessian[self.n:, self.n:] += (
+                        landmarks_prior_weight *
+                        self.compute_hessian(sd_lms_camera))
+                    sd_error[self.n:] += (
+                        landmarks_prior_weight *
+                        self.compute_sd_error(sd_lms_camera, lms_error))
+
+            if return_costs:
+                costs.append(self.compute_cost(
+                    img_error_uv, lms_error, shape_parameters,
+                    texture_parameters, shape_prior_weight,
+                    texture_prior_weight, landmarks_prior_weight))
+
+            # Solve to find the increment of parameters
+            d_shape, d_camera = self.solve(hessian, sd_error, camera_update,
+                                           focal_length_update)
+
+            # Update parameters
+            shape_parameters += d_shape
+            if camera_update:
+                camera_parameters = camera_parameters_update(
+                    camera_parameters, d_camera)
+                camera = camera.from_vector(camera_parameters)
+
+            # Generate the updated instance
+            instance = self.model.instance(shape_weights=shape_parameters,
+                                           texture_weights=texture_parameters)
+
+            # Update lists
+            shape_parameters_per_iter.append(shape_parameters)
+            texture_parameters_per_iter.append(texture_parameters)
+            camera_per_iter.append(camera)
+            instance_per_iter.append(instance.with_rescaled_texture(0., 1.))
+
+            # Increase iteration counter
+            k += 1
+
+            # shape_parameters, texture_parameters, camera_parameters = yield \
+            #     shape_parameters, texture_parameters, camera_parameters
+
+        return MMAlgorithmResult(
+            shape_parameters=shape_parameters_per_iter,
+            texture_parameters=texture_parameters_per_iter,
+            meshes=instance_per_iter, camera_transforms=camera_per_iter,
+            image=image, initial_mesh=initial_mesh.with_rescaled_texture(0., 1.),
+            initial_camera_transform=camera_per_iter[0], gt_mesh=gt_mesh,
+            costs=costs)
+
+    def project_out(self, J):
+        tmp = self.model.texture_model.components.dot(J)
+        return J - self.texture_T.dot(tmp)
+
+    def compute_hessian(self, sd):
+        n_params = sd.shape[1]
+        sd = np.transpose(sd, (1, 0, 2)).reshape(n_params, -1)
+        return sd.dot(sd.T)
+
+    def J_data(self, camera, warped_uv, shape_pc_uv, grad_x_uv, grad_y_uv,
+               camera_update, focal_length_update):
+        # Compute derivative of camera wrt shape and camera parameters
+        dp_da_dr = d_camera_d_shape_parameters(camera, warped_uv, shape_pc_uv)
+        n_camera_parameters = 0
+        if camera_update:
+            dp_dr = d_camera_d_camera_parameters(
+                camera, warped_uv, with_focal_length=focal_length_update)
+            dp_da_dr = np.hstack((dp_da_dr, dp_dr))
+            n_camera_parameters = dp_dr.shape[1]
+
+        # Multiply image gradient with camera derivative
+        permuted_grad_x = np.transpose(grad_x_uv[..., None], (0, 2, 1))
+        permuted_grad_y = np.transpose(grad_y_uv[..., None], (0, 2, 1))
+        J = permuted_grad_x * dp_da_dr[0] + permuted_grad_y * dp_da_dr[1]
+
+        # Project-out
+        n_params = J.shape[1]
+        J = np.transpose(J, (1, 0, 2)).reshape(n_params, -1)
+        PJ = self.project_out(J)
+
+        # Concatenate to create the data term steepest descent
+        return PJ, n_camera_parameters
+
+    def solve(self, hessian, sd_error, camera_update, focal_length_update):
+        # Solve
+        ds = - np.linalg.solve(hessian, sd_error)
+
+        # Get shape parameters increment
+        d_shape = ds[:self.n]
+
+        # Get camera parameters increment
+        if camera_update:
+            # Keep the rest
+            ds = ds[self.n:]
+
+            # If focal length is not updated, then set its increment to zero
+            if not focal_length_update:
+                ds = np.insert(ds, 0, [0.])
+
+            # Set increment of the 1st quaternion to one
+            ds = np.insert(ds, 1, [1.])
+
+            # Get camera parameters update
+            d_camera = ds
+        else:
+            d_camera = None
+
+        return d_shape, d_camera
+
+    def _precompute(self):
+        # call super method
+        super(WibergForwardAdditive, self)._precompute()
+        self.texture_T = self.model.texture_model.components.T
+
+    def __str__(self):
+        return "Wiberg Forward Additive"
 
 
 def quaternion_multiply(current_q, increment_q):
