fec5dcd95d406c17274205ad189806c084399f00
Patrick Snape <patricksnape@gmail.com>
2019-08-01 12:56:48 +0200

    Expose nicp at the package level

--- menpo3d/__init__.py
+++ menpo3d/__init__.py
@@ -1,10 +1,12 @@
+from . import barycentric
+from . import correspond
 from . import io
 from . import rasterize
 from . import unwrap
 from . import visualize
 from . import vtkutils
-from . import barycentric
 
 from ._version import get_versions
+
 __version__ = get_versions()['version']
 del get_versions
