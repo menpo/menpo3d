ac17cd0c526c2f23d77ceda652bbda0579270dd4
Thanos <athanasios.papaioannou@huawei.com>
2020-09-16 10:40:49 +0000

    Add view method for 3D Shape Model - Use K3D and LinearModelParametersWidget from menpowidget - Support of landmarks in shape model

--- menpo3d/visualize/__init__.py
+++ menpo3d/visualize/__init__.py
@@ -2,4 +2,4 @@ from .base import (PointGraphViewer3d, TriMeshViewer3d, VectorViewer3d,
                    ColouredTriMeshViewer3d, TexturedTriMeshViewer3d,
                    LandmarkViewer3d, TriMeshInlineViewer3d,
                    PointGraphInlineViewer3d, TexturedTriMeshInlineViewer3d,
-                   LandmarkInlineViewer3d)
+                   LandmarkInlineViewer3d, PCAModelInlineViewer3d)
--- menpo3d/visualize/base.py
+++ menpo3d/visualize/base.py
@@ -9,7 +9,8 @@ from .viewmayavi import (
 from .viewk3dwidgets import (K3dwidgetsTriMeshViewer3d,
                              K3dwidgetsPointGraphViewer3d,
                              K3dwidgetsLandmarkViewer3d,
-                             K3dwidgetsTexturedTriMeshViewer3d)
+                             K3dwidgetsTexturedTriMeshViewer3d,
+                             K3dwidgetsPCAModelViewer3d)
 
 PointGraphViewer3d = MayaviPointGraphViewer3d
 TriMeshViewer3d = MayaviTriMeshViewer3d
@@ -22,3 +23,4 @@ TriMeshInlineViewer3d = K3dwidgetsTriMeshViewer3d
 TexturedTriMeshInlineViewer3d = K3dwidgetsTexturedTriMeshViewer3d
 LandmarkInlineViewer3d = K3dwidgetsLandmarkViewer3d
 PointGraphInlineViewer3d = K3dwidgetsPointGraphViewer3d
+PCAModelInlineViewer3d = K3dwidgetsPCAModelViewer3d
