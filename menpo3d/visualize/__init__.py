c8b83b5f2bfbfd997ab188a9369ffef4647b4acf
Thanos <athanasios.papaioannou@huawei.com>
2021-01-17 17:09:16 +0000

    Fix error in import Heatmap3d in visualize/__init__.py

--- menpo3d/visualize/__init__.py
+++ menpo3d/visualize/__init__.py
@@ -1,6 +1,6 @@
 from .base import (PointGraphViewer3d, TriMeshViewer3d, VectorViewer3d,
                    ColouredTriMeshViewer3d, TexturedTriMeshViewer3d,
-                   LandmarkViewer3d, MayaviHeatmapViewer3d,
+                   LandmarkViewer3d, HeatmapViewer3d,
                    TriMeshInlineViewer3d, ColouredTriMeshInlineViewer3d,
                    PointGraphInlineViewer3d, TexturedTriMeshInlineViewer3d,
                    LandmarkInlineViewer3d, PointGraphInlineViewer3d,
